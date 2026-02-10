a5d9332a62370a28ee06811f7bc31aa266061ddc
KerolosMaged <kerolosmaged2050>
2026-02-10 19:05:58 +0200

    update

--- GPIO/GPIO_CONFGR.h
+++ GPIO/GPIO_CONFGR.h
@@ -8,22 +8,27 @@ Github      : https://github.com/KerolosMaged
 #ifndef GPIO_CONFGR_H
 #define GPIO_CONFGR_H
 
+/*========== Define Pins =========*/
+#define  PIN0    0
+#define  PIN1    1
+#define  PIN2    2
+#define  PIN3    3
+#define  PIN4    4
+#define  PIN5    5
+#define  PIN6    6
+#define  PIN7    7
+#define  PIN8    8
+#define  PIN9    9
+#define  PIN10   10
+#define  PIN11   11
+#define  PIN12   12
+#define  PIN13   13
+#define  PIN14   14
+#define  PIN15   15
+/*================================*/
 
-#define PIN0    0
-#define PIN1    1
-#define PIN2    2
-#define PIN3    3
-#define PIN4    4
-#define PIN5    5
-#define PIN6    6
-#define PIN7    7
-#define PIN8    8
-#define PIN9    9
-#define PIN10   10
-#define PIN11   11
-#define PIN12   12
-#define PIN13   13
-#define PIN14   14
-#define PIN15   15
+
+
+
 
 #endif
