a669be6986e10c6529661080919e6090dd72e6ff
KerolosMaged <159344072+KerolosMaged@users.noreply.github.com>
2026-03-15 01:45:52 +0200

    MORE FOR NVIC

---
+++ NVIC/NVIC_DRIVER.c
@@ -0,0 +1,24 @@
+/*========================================================
+File        : NVIC_INTERFACE
+Description : This file contain the definetions of NVIC functions and source file of NVIC
+Author      : Kerolos Maged Amin Fekry
+Date        : 9/2/2026
+Github      : https://github.com/KerolosMaged
+========================================================*/
+
+/********** Include STD_TYPES_MATH file ***********/
+#include "../LIB/STD_TYPES_MATH.h"
+/*********** Include of RCC files  **********/
+#include "../RCC/RCC_CONFGR.h"
+#include "../RCC/RCC_INTERFACE.h"
+#include "../RCC/RCC_PRIVATE.h"
+/*********** Include of NVIC files  **********/
+#include "NVIC_CONFGR.h"
+#include "NVIC_INTERFACE.h"
+#include "NVIC_PRIVATE.h"
+
+
+
+void NVIC_EnableIRQ(IRQn_Type IRQn){
+    
+}
--- NVIC/NVIC_INTERFACE.h
+++ NVIC/NVIC_INTERFACE.h
@@ -1,3 +1,10 @@
+/*========================================================
+File        : NVIC_INTERFACE
+Description : This file contain the declerations of NVIC functions 
+Author      : Kerolos Maged Amin Fekry
+Date        : 9/2/2026
+Github      : https://github.com/KerolosMaged
+========================================================*/
 #ifndef NVIC_INTERFACE_H
 #define NVIC_INTERFACE_H
 
@@ -9,6 +16,8 @@ void NVIC_DisableIRQ(IRQn_Type IRQn);
 
 void NVIC_SetPendingIRQ(IRQn_Type IRQn);
 
+uint32_t NVIC_GetPendingIRQ (IRQn_t IRQn)
+
 void NVIC_ClearPendingIRQ(IRQn_Type IRQn);
 
 uint32_t NVIC_GetPendingIRQ(IRQn_Type IRQn);
@@ -17,7 +26,7 @@ void NVIC_SetPriority(IRQn_Type IRQn, uint32_t priority);
 
 uint32_t NVIC_GetPriority(IRQn_Type IRQn);
 
-
+void NVIC_SystemReset (void);
 
 
 #endif
